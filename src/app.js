f570154b7d461ae20f9559bf3b80a2afc93f7165
lucax0 <lucax.uesato@gmail.com>
2020-07-23 15:35:23 -0300

    add rotas faltando

--- src/app.js
+++ src/app.js
@@ -20,7 +20,16 @@ function logRequests(request, response, next) {
     console.timeEnd(log);
 }
 
-mongoDB.saveVagas();
+function validateUId(request, response, next){
+    const { id} = request.params;
+  
+    if(!isUuid(id)){
+      return response.status(400).json({ "error": "Invalid Repositorie ID" });
+    }
+  
+    return next();
+  }
+
 
 app.use(logRequests)
 
@@ -34,28 +43,41 @@ app.post("/vagas", (request, response) => {
     const { titulo, descricao, data_limite, numero_vagas } = request.body;
     const vaga = { id: uuid(), titulo: titulo, descricao: descricao,data_limite: data_limite, numero_vagas: numero_vagas, candidatos : []};
 
+    mongoDB.saveVagas(vaga);
+
     return response.json(vaga);
 });
 
-app.put("/vagas/:id", (request, response) => {
+app.put("/vagas/:id",validateUId, (request, response) => {
     const { id } = request.params;
     const { titulo, descricao, data_limite, numero_vagas } = request.body;
     return response.json();
 });
 
-app.delete("/vagas/:id", (request, response) => {
+app.delete("/vagas/:id",validateUId, (request, response) => {
     const { id } = request.params;
     return response.status(204).send();
 })
 
 //Canditados
 
-app.get("/candidatos", (request, response) => {
+app.post("/candidatos", (request, response) => {
     const { cpf, nome, email, senha } = request.body;
     return response.json();
 });
 
-app.delete("/candidatos/:id", (request, response) => {
+app.get("/candidatos", (request, response) => {
+    return response.json();
+});
+
+app.put("/candidatos/:id",validateUId, (request, response) => {
+    const { id } = request.params;
+    const { nome,senha } = request.body;
+    return response.json();
+});
+
+
+app.delete("/candidatos/:id",validateUId, (request, response) => {
     const { id } = request.params;
     return response.status(204).send();
 })
--- src/mongoDB.js
+++ src/mongoDB.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://apiTest:743xBtWtHSFLtSCC@cluster0-fcpmn.gcp.mongodb.net/matchbox?authMechanism=SCRAM-SHA-1', {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = {
- saveVagas: async function () {
+ saveVagas: async function (vaga) {
+
     const Vaga = mongoose.model('vagas', { id: String, titulo: String, descricao: String,data_limite: Date, numero_vagas: String, candidatos : Array});// Definindo uma model "Formato" do objeto para ser salvo no mongodDB
 
-
-    const vagas = new Vaga({ titulo: 'Teste', data_limite: 20/07/1995 });
+    const vagas = new Vaga({ vaga });
     vagas.save().then(() => console.log('Vaga OK'));
 },
 
