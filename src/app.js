4ac479c0e2d919c951832846724b2e0f33b7465c
lucax0 <lucax.uesato@gmail.com>
2020-07-28 03:49:32 -0300

    CRUD vagas OK

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // Lucas Uesato
 // Teste MatchBox
 const express = require('express');
-const { uuid , isUuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 const mongoDB = require('./mongoDB')
 
 const { request, response } = require('express');
@@ -20,80 +20,106 @@ function logRequests(request, response, next) {
     console.timeEnd(log);
 }
 
-function validateUId(request, response, next){
-    const { id} = request.params;
-  
-    if(!isUuid(id)){
-      return response.status(400).json({ "error": "Invalid ID" });
+function validateUId(request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ "error": "Invalid ID" });
     }
-  
+
     return next();
-  }
+}
 
 
 app.use(logRequests)
 
-//Vagas
+//VAGAS
+//Econtrar todas as vagas
+app.get("/vagas", async (request, response) => {
+    try {
+        const results = await mongoDB.getAllByType('v');
+        return response.json(results);
+    } catch (error) {
+        return response.status(400).json({ "error": `Error co consolidar DB -> ${error}` });
+    }
+});
+// Encontrar 1 vaga
+app.get("/vagas/:id", validateUId, async (request, response) => {
+    const { id } = request.params;
+    const result = await mongoDB.getOneByType(id, 'v');
 
-app.get("/vagas", (request, response) => {
-    return response.json();
+    return response.json(result);
 });
 
 app.post("/vagas", (request, response) => {
     const { titulo, descricao, data_limite, numero_vagas } = request.body;
-    const data = { id: uuid(), titulo: titulo, descricao: descricao,data_limite: data_limite, numero_vagas: numero_vagas, candidatos : []};
+    const data = { id: uuid(), titulo: titulo, descricao: descricao, data_limite: data_limite, numero_vagas: numero_vagas, candidatos: [] };
 
-    mongoDB.saveVagas(data);
+    mongoDB.setVagas(data);
 
-    return response.json(data);
+    return response.status(201).json(data);
 });
 
 //Adicionar o candidato a uma ou mais vagas
-app.post("/vagas/addcandidato/:idcand",validateUId, (request, response) => {
+app.post("/vagas/addcandidato/:id", validateUId, (request, response) => {
     const { idcand } = request.params;
     return response.json();
 });
 
 //Remover o candidato a vaga
-app.post("/vagas/:idvaga/removecandidato/:idcand",validateUId, (request, response) => {
+app.post("/vagas/:idvaga/removecandidato/:idcand", validateUId, (request, response) => {
     const { idvaga, idcand } = request.params;
     return response.status(204).send();
 });
 
-app.put("/vagas/:id",validateUId, (request, response) => {
+app.put("/vagas/:id", validateUId, (request, response) => {
     const { id } = request.params;
     const { titulo, descricao, data_limite, numero_vagas } = request.body;
-    return response.json();
+    const data = { titulo: titulo, descricao: descricao, data_limite: data_limite, numero_vagas: numero_vagas }
+
+    try {
+        const result = mongoDB.updateVagas(id, data);
+        return response.json({ "sucess": `Update OK on ID -> ${id}` });
+    } catch (error) { 
+        return response.status(400).json({ "error": `Error co consolidar DB -> ${error}` });
+    }
+
 });
 
-app.delete("/vagas/:id",validateUId, (request, response) => {
+app.delete("/vagas/:id", validateUId, (request, response) => {
     const { id } = request.params;
-    return response.status(204).send();
+    try {
+        mongoDB.deleteOneByType(id , 'v')
+        return response.status(204).send();
+    } catch (error) {
+        return response.status(400).json({ "error": `Error co consolidar DB -> ${error}` });
+    }
+    
 })
 
-//Candidados
+//CANDIDATOS
 
-app.post("/candidatos", (request, response) => {    
+app.post("/candidatos", (request, response) => {
     const { cpf, nome, email, senha } = request.body;
-    const data = {id : uuid(), cpf: cpf, nome: nome, email:email, senha:senha}
+    const data = { id: uuid(), cpf: cpf, nome: nome, email: email, senha: senha }
 
-    mongoDB.saveCandidatos(data);
+    mongoDB.setCandidatos(data);
 
-    return response.json(data);
+    return response.status(201).json(data);
 });
 
 app.get("/candidatos", (request, response) => {
     return response.json();
 });
 
-app.put("/candidatos/:id",validateUId, (request, response) => {
+app.put("/candidatos/:id", validateUId, (request, response) => {
     const { id } = request.params;
-    const { nome,senha } = request.body;
+    const { nome, senha } = request.body;
     return response.json();
 });
 
 
-app.delete("/candidatos/:id",validateUId, (request, response) => {
+app.delete("/candidatos/:id", validateUId, (request, response) => {
     const { id } = request.params;
     return response.status(204).send();
 })
--- src/mongoDB.js
+++ src/mongoDB.js
@@ -2,26 +2,43 @@
 const mongoose = require('mongoose');
 
 const mongooseString = 'mongodb+srv://apiTest:743xBtWtHSFLtSCC@cluster0-fcpmn.gcp.mongodb.net/matchbox?authMechanism=SCRAM-SHA-1';
-
-const Vaga = mongoose.model('vagas', { id: String, titulo: String, descricao: String,data_limite: Date, numero_vagas: String, candidatos : Array});
+const VagaModel = mongoose.model('vagas', { id: String, titulo: String, descricao: String, data_limite: Date, numero_vagas: String, candidatos: Array });
 // Definindo uma model "Formato" do objeto para ser salvo no mongodDB
-const CandidatoModel = mongoose.model('candidatos', {id: String, cpf: String, nome: String, email: String, senha: String } );
+const CandidatoModel = mongoose.model('candidatos', { id: String, cpf: String, nome: String, email: String, senha: String });
 // MODEL DEVE SER UMA VAR GLOBAL SE N EM TD CHAMADA ELE TENTA CRIAR UM SCHEMA
 
-mongoose.connect(mongooseString , {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongooseString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = {
+
+    setVagas: async function (data) {
+        const vagas = new VagaModel(data);
+
+        vagas.save().then(() => { console.log('Vaga OK'); });
+    },
+
+    updateVagas: async function(id,data) {
+        const result = VagaModel.findOneAndUpdate({id: id},data);
+        return result
+    },
+
+    setCandidatos: async function (data) {
+        const candidato = new CandidatoModel(data);
+
+        candidato.save().then(() => console.log('Candidato OK'));
+    },
+
+    getAllByType: async function (type) {
+        //Dessa forma economiza linhas de codigo sendo que uma mesma function pesquisa para os 2
+        return type == "v" ? await VagaModel.find() : await CandidatoModel.find();
+    },
     
- saveVagas: async function (data) {
-    const vagas = new Vaga(data);
+    getOneByType: async function (id, type) {
+        return type == "v" ? await VagaModel.find({ id: id }) : await CandidatoModel.find({ id: id });
+    },
 
-    vagas.save().then(() => {console.log('Vaga OK');});
-},
-
-saveCandidatos: async function (data) {
-
-    const candidato = new CandidatoModel(data);
-    candidato.save().then(() => console.log('Candidato OK'));
-}
+    deleteOneByType: async function (id, type) {
+        return type == "v" ? await VagaModel.findOneAndDelete({ id: id }) : await CandidatoModel.findOneAndDelete({ id: id });
+    }
 
 }
